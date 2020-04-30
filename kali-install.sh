969785ad4b6a8555ec2fa65104d9e0ea15176837
DaWoschbar <ffd94f449e94055d3eb2f94cb1c327250af9c657913637091967ef97b5dc82e1>
2020-04-30 13:02:46 +0200

    Fixed issues, tidying up and added autoinstall

--- kali-install.sh
+++ kali-install.sh
@@ -8,21 +8,22 @@ https://github.com/PowerShellMafia/PowerSploit.git
 https://github.com/SecureAuthCorp/impacket.git
 https://github.com/pentestmonkey/php-reverse-shell.git
 https://github.com/fox-it/mitm6.git
-https://github.com/Hackplayers/evil-winrm.git
 https://github.com/samratashok/nishang.git
 https://github.com/trustedsec/unicorn.git
-https://github.com/SecureAuthCorp/impacket.git
 )
 
 GIT_PYTHON=(
 https://github.com/SecureAuthCorp/impacket.git
 )
 
+ARRAY_RUBY=(
+https://github.com/Hackplayers/evil-winrm.git
+)
+
 ARRAY_APT=(
 gobuster
 hexedit
 tmux
-python-pip
 python3-pip
 xclip
 crackmapexec
@@ -33,14 +34,13 @@ ARRAY_FOLDER=(
 vh
 overTheWire
 htb
-CTFs/picoCTF19
 )
 
 function prep_repos()
 {
-	echo -e "\e[93mInstalling git repos..."
+	echo -e "\e[93m Installing git repos..."
 
-	read -r -p "Should the existing repos in /opt/ updated before the install? [Y/n] " response
+	read -r -p "Should the existing repos in /opt/ updated before the install? [y/N] " response
 	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 	then
 		update_repos
@@ -51,47 +51,112 @@ function prep_repos()
 		basename=$(basename $i)
 		repo_name=${basename%.*}
 
-		echo " => \e[32mInstalling ${repo_name}"
-		git clone -q $i /opt/$repo_name
-		echo "Installed $repo_name successfully!"
-
+		if [[ -d "/opt/$repo_name" ]]
+		then
+			echo "$repo_name already exists in /opt!"
+		else
+			echo " => \e[32m Installing ${repo_name}"
+			git clone -q $i /opt/$repo_name
+			echo "Installed $repo_name successfully!"
+		fi
 	done
 
-	echo -e "\e[92mFinished Repo Download!"
+	for i in "${GIT_PYTHON[@]}"
+	do
+		basename=$(basename $i)
+		repo_name=${basename%.*}
+
+		if [[ -d "/opt/$repo_name" ]]
+		then
+			echo "$repo_name already exists in /opt!"
+		else
+			echo " => \e[32mInstalling ${repo_name}"
+			git clone -q $i /opt/$repo_name
+			pip3 install -r /opt/$repo_name/requirements.txt > /dev/null
+			python3 /opt/$repo_name/setup.py install > /dev/null
+			echo "Installed $repo_name successfully!"
+		fi		
+	done
+
+	echo -e "\e[92m Finished Repo Download!"
 }
 
 function update_repos()
 {
 	echo -e "\e[93mUpdating git repos"
-	for d in /opt/*; do cd $d; git stash --quiet; (git pull --quiet &); cd ..; done
-
+	
+	for d in /opt/*
+	do
+		cd $d;
+		if [[ -d $d/.git ]]
+		then
+			echo " => Updating $d"; git stash --quiet; (git pull --quiet &); cd ..;
+		fi
+	done
 	echo -e "\e[92mFinished repo updates!"
 }
 
 function prep_apt()
 {
-	echo -e "\e[93mInstalling Aptitude packages"
+	echo -e "\e[93m Installing Aptitude packages"
 	echo "update APTITUDE"
-	apt update
+	apt-get update > /dev/null
 	for i in "${ARRAY_APT[@]}"; do
-		echo -e " => \e[93mInstalling ${i}"
-		apt install $i -y -qq
+		echo -e " => \e[93m Installing ${i}"
+		apt-get install $i -y -qq >/dev/null
 	done
 
-	echo -e "\e[92mFinished download apt packages"
+	echo -e "\e[93m Performing apt full-upgrade..."
+	apt-get full-upgrade -y -qq
+	echo -e "\e[93m Performing apt autoremove..."
+	apt-get autoremove -y -qq
+
+	echo -e "\e[92m Finished download apt packages"
 }
 
 function update_apt()
 {
 	echo "Updating Aptitude packages"
 	echo "update APTITUDE"
-	apt update
+	apt-get update -qq
 	for i in "${ARRAY_APT[@]}"; do
-		echo ">>installing ${i}"
-		apt update $i -y -qq
+		echo "=> Updating ${i}"
+		apt-get update $i -y -qq
 	done
 
-	echo -e "\e[92mFinished apt update"
+	echo -e "\e[92m Finished apt update"
+
+	echo "Running apt autoremove..."
+	apt-get autoremove -y -qq
+}
+
+function prep_ruby()
+{
+	echo -e "\e[93m Installing Ruby gem packages"
+	
+	for i in "${ARRAY_RUBY[@]}"
+	do
+		basename=$(basename $i)
+		repo_name=${basename%.*}
+
+		echo -e " => \e[32mInstalling ${repo_name}"
+		gem install $basename --silent
+		echo "Installed $repo_name successfully!"
+
+	done
+}
+
+function update_ruby()
+{
+	for i in "${ARRAY_RUBY[@]}"
+	do
+		basename=$(basename $i)
+		repo_name=${basename%.*}
+
+		echo -e " => \e[32m Updating ${repo_name}"
+		gem update $basename --silent
+	done
+	echo -e "Finished ruby updates"
 }
 
 function prep_shell_env()
@@ -109,10 +174,9 @@ function do_misc()
 {
 	echo "Executing misc"
 	echo "Removing unnecessary folders..."
-	rmdir rmdir ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Videos/ ~/Templates/
+	rmdir rmdir ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Videos/ ~/Templates/ 2> /dev/null
 
 	echo "Adding system links from shared folder in the home directory"
-
 	shared-folder-syslinks
 	
 	read -r -p "Should Atom be installed? [y/N] " response
@@ -123,7 +187,7 @@ function do_misc()
 		dpkg -i /tmp/atom.deb
 	fi
 
-	read -r -p "Should autologon be enabled? - recommended on VMs [y/N] " response
+	read -r -p "Should autologon be enabled? [y/N] " response
 	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 	then
 		echo "AutomaticLoginEnable = true" >> /etc/gdm3/daemon.conf
@@ -146,10 +210,8 @@ function to_do()
 {
 	echo "****Consider the following actions to be implemented in the future... ****"
 	echo "- APT: Stegosolve, stegohide"
-	echo "- Windows Exploitation Repos"
 	echo "- Automatic installation of some installed git repos"
 	echo "- Install FoxyProxy and try to configure it automatically (if possible)"
-	echo "- autologin in virtual machine"
 	echo "- OneDrive Shared folder"
 	echo "- colored output"
 }
@@ -157,9 +219,8 @@ function to_do()
 function usage ()
 {
 
-	echo "-u 	--update	 		Update repos & aptitude packages."
+	echo "-u 	--update	 		Update git repos & aptitude packages ."
 	echo "-s 	--shell-env	 		Only prep ZSH-Shell env"
-	echo "-g 	--git-only			Install git repos only."
 	echo "-apt 	--apt-only			Install only apt packages. Apt update included."
 	echo "-sf	--shared-folders	-sf | --shared-folders 			Install VMware tools and add softlinks of shared folders to it."
 	echo "-m 	--misc-only			Execute misc tasks like removing home folders."
@@ -169,24 +230,19 @@ function usage ()
 	echo "-h 	--help 				This help page."
 }
 
-function to_imp ()
-{
-	echo "currently not implemented"
-}
-
 function shared-folder-syslinks ()
 {
-	echo "Running shared folder VMware script..."
-	dir=$(pwd)
-	cp dir/mount-shared-folders ~/Desktop/
-	~/mount-shared-folders
+	#echo "Running shared folder VMware script..."	
+	#cp /opt/kali-setup/mount-shared-folders ~/Desktop/
 
 	echo "Creating sys links from shared folders..."
 	for i in "${ARRAY_FOLDER[@]}"
 	do
-		echo "creating $i"
-		echo $i
-		ln -s /mnt/hgfs/Hacking/$i ~/$i
+		if [[ ! -d ~/$i ]]
+		then
+			echo "Creating $i"
+			ln -s /mnt/hgfs/Hacking/$i ~/$i
+		fi
 	done
 }
 
@@ -194,7 +250,15 @@ function full_install ()
 {
 	prep_repos
 	prep_apt
+	prep_ruby
 	do_misc
+}
+
+function full_update ()
+{
+	update_repos 
+	update_apt 
+	update_ruby
 }
 
 if [[ $# -eq 0 ]] ; then
@@ -212,7 +276,7 @@ while [ "$1" != "" ]; do
         -h | --help ) usage
 		exit;;
 
-		-u | --update )	update_repos update_apt
+		-u | --update )	full_update
 		exit;;
 		-s | --shell-env )	prep_shell_env
 		exit;;
@@ -226,7 +290,7 @@ while [ "$1" != "" ]; do
 		-m | --misc-only )	do_misc
 		exit;;
 
-		-sf | --shared-folders )	vmware-tools shared-folder-syslinks
+		-sf | --shared-folders ) shared-folder-syslinks
 		exit;;
 
 		-sl | --soft-links ) shared-folder-syslinks
@@ -243,3 +307,6 @@ while [ "$1" != "" ]; do
     esac
     shift
 done
+
+
+echo -e "Script ended."
